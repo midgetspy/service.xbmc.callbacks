03fcb8d30351197617809722e607cdb7fc503853
Nic Wolfe <nic@wolfeden.ca>
2015-11-05 15:36:53 -0700

    Transitioning to my rewrite, working version of video events only

--- default.py
+++ default.py
@@ -19,22 +19,8 @@
 #    This script is based on script.randomitems & script.wacthlist
 #    Thanks to their original authors
 
-import os
-import sys
 import xbmc
-import xbmcgui
 import xbmcaddon
-import subprocess
-
-script_xbmc_starts = ''
-script_player_starts = ''
-script_player_stops = ''
-script_player_pauses = ''
-script_player_resumes = ''
-script_screensaver_starts = ''
-script_screensaver_stops = ''
-script_idle = ''
-script_db_update = ''
 
 __addon__        = xbmcaddon.Addon()
 __addonversion__ = __addon__.getAddonInfo('version')
@@ -43,206 +29,129 @@ __addonname__    = __addon__.getAddonInfo('name')
 
 def log(txt):
     message = '%s: %s' % (__addonname__, txt.encode('ascii', 'ignore'))
-    xbmc.log(msg=message, level=xbmc.LOGDEBUG)
+    xbmc.log(msg=message, level=xbmc.LOGNOTICE)
+
+class MediaTypes:
+    MOVIE = "MOVIE"
+    AUDIO = "AUDIO"
+    EPISODE = "EPISODE"
+    VIDEO = "VIDEO"
+    TRAILER = "TRAILER"
+    UNKNOWN = "UNKNOWN"
+    
+    @staticmethod
+    def isVideo(mediaType):
+        return mediaType in (MediaTypes.MOVIE, MediaTypes.EPISODE, MediaTypes.VIDEO, MediaTypes.TRAILER)
+
+class StereoscopicModes:
+    NONE = "2D"
+    HSBS = "HSBS"
+    
+    @staticmethod
+    def getMode(xbmcValue):
+        if xbmcValue == '':
+            return StereoscopicModes.NONE
+        elif xbmcValue == 'left_right':
+            return StereoscopicModes.HSBS
+
+class AspectRatios:
+    DEFAULT = 'DEFAULT'
+    # 1.33, 1.37, 1.66, 1.78, 1.85, 2.20, 2.35, 2.40, 2.55, 2.76
+
+class EventNames:
+    PLAYING = "PLAYING"
+    PAUSED = "PAUSED"
+    RESUMED = "RESUMED"
+    STOPPED = "STOPPED"
 
 class Main:
-  def __init__(self):
-    self._init_vars()
-    self._init_property()
-    global script_xbmc_starts
-    if script_xbmc_starts:
-      log('Going to execute script = "' + script_xbmc_starts + '"')
-      try:
-          subprocess.call(script_xbmc_starts)
-      except:
-          log('Error executing script when xbmc starts')
-    self._daemon()
+    def __init__(self):
+        self.playerEventReceiver = PlayerEventReceiver()
+        self._daemon()
+        
+    def _daemon(self):
+        while not xbmc.abortRequested:
+            xbmc.sleep(1000)
+        log('abort requested')
+        
+class PlayerEventReceiver(xbmc.Player):
+    curMediaType = None
+    
+    def _sendSignal(self, event):
+        mediaType = self._getMediaType()
+        
+        if event == EventNames.PLAYING:
+            self.curMediaType = mediaType
+        elif event == EventNames.STOPPED:
+            mediaType = self.curMediaType
+            self.curMediaType = None
+    
+        if not MediaTypes.isVideo(mediaType):
+            log("Non-video media played, not sending any signals: " + mediaType)
+            return
 
-  def _init_vars(self):
-    self.Player = MyPlayer()
-    self.Monitor = MyMonitor(update_settings = self._init_property, player_status = self._player_status)
+        stereoscopic = self._getStereoscopicMode()
+        aspectRatio = self._getAspectRatio()
+        
+        log("Sending {event}, {mediaType!r} {aspectRatio!r} {stereoscopic!r}".format(event=event, mediaType=mediaType, aspectRatio=aspectRatio, stereoscopic=stereoscopic))
 
-  def _init_property(self):
-    log('Reading properties')
-    global script_xbmc_starts
-    global script_player_starts
-    global script_player_stops
-    global script_player_pauses
-    global script_player_resumes
-    global script_screensaver_starts
-    global script_screensaver_stops
-    global script_idle
-    global script_db_update
-    script_xbmc_starts = xbmc.translatePath(__addon__.getSetting("xbmc_starts"))
-    script_player_starts = xbmc.translatePath(__addon__.getSetting("player_starts"))
-    script_player_stops = xbmc.translatePath(__addon__.getSetting("player_stops"))
-    script_player_pauses = xbmc.translatePath(__addon__.getSetting("player_pauses"))
-    script_player_resumes = xbmc.translatePath(__addon__.getSetting("player_resumes"))
-    script_screensaver_starts = xbmc.translatePath(__addon__.getSetting("screensaver_starts"))
-    script_screensaver_stops = xbmc.translatePath(__addon__.getSetting("screensaver_stops"))
-    script_idle = xbmc.translatePath(__addon__.getSetting("xbmc_idle"))
-    script_db_update = xbmc.translatePath(__addon__.getSetting("db_update"))
-    log('script xbmc starts = "' + script_xbmc_starts + '"')
-    log('script player starts = "' + script_player_starts + '"')
-    log('script player stops = "' + script_player_stops + '"')
-    log('script player pauses = "' + script_player_pauses + '"')
-    log('script player resumes = "' + script_player_resumes + '"')
-    log('script screensaver starts = "' + script_screensaver_starts + '"')
-    log('script screensaver stops = "' + script_screensaver_stops + '"')
-    log('script idle = "' + script_idle + '"')
-    log('db update = "' + script_db_update + '"')
+    def _getStereoscopicMode(self):
+        return StereoscopicModes.getMode(xbmc.getInfoLabel('VideoPlayer.StereoscopicMode'))
+        
+    def _getAspectRatio(self):
+        return xbmc.getInfoLabel('VideoPlayer.VideoAspect') or AspectRatios.DEFAULT
+        
+    def _getMediaType(self):
+        mediaType = MediaTypes.UNKNOWN
 
-  def _player_status(self):
-    return self.Player.playing_status()
+        if self.isPlayingAudio():
+            mediaType = MediaTypes.MUSIC
+        elif self.isPlayingVideo():
+            
+            for i in range(10):
+                if xbmc.getInfoLabel('VideoPlayer.Title') != '':
+                    break
 
-  def _daemon(self):
-    while (not xbmc.abortRequested):
-      # Do nothing
-      global script_idle
-      if script_idle:
-        if xbmc.getGlobalIdleTime() > 60 * __addon__.getSetting("idle_time"):
-          log('XBMC is idle')
-          log('Going to execute script = "' + script_idle + '"')
-          try:
-              subprocess.call(script_idle)
-          except:
-              log('ERROR executing script when xbmc goes idle')
-      xbmc.sleep(4000)
-    log('abort requested')
+                log("Player is not ready on attempt {}, waiting 10ms and trying again".format(i))
+                xbmc.sleep(10)
 
+            if xbmc.getCondVisibility('VideoPlayer.Content(movies)'):
+                try:
+                    filename = self.getPlayingFile()
+                    if '-trailer' in filename:
+                        mediaType = MediaTypes.TRAILER
+                    elif filename.startswith('http://') or filename.startswith('https://'):
+                        mediaType = MediaTypes.TRAILER
+                    else:
+                        mediaType = MediaTypes.MOVIE
+                except:
+                    log("Exception trying to get the current filename")
+                    pass
+            elif xbmc.getCondVisibility('VideoPlayer.Content(episodes)') and xbmc.getInfoLabel('VideoPlayer.Season') != '' and xbmc.getInfoLabel('VideoPlayer.TVShowTitle') != '':
+                mediaType = MediaTypes.EPISODE
+            else:
+                mediaType = MediaTypes.VIDEO
+        else:
+            log('Unknown media type currently playing')
+            
+        return mediaType
 
-class MyMonitor(xbmc.Monitor):
-  def __init__(self, *args, **kwargs):
-    xbmc.Monitor.__init__(self)
-    self.get_player_status = kwargs['player_status']
-    self.update_settings = kwargs['update_settings']
+    def onPlayBackStarted(self):
+        self._sendSignal(EventNames.PLAYING)
 
-  def onSettingsChanged(self):
-    self.update_settings()
+    def onPlayBackEnded(self):
+        self.onPlayBackStopped()
 
-  def onScreensaverActivated(self):
-    log('screensaver starts')
-    global script_screensaver_starts
-    if script_screensaver_starts:
-      log('Going to execute script = "' + script_screensaver_starts + '"')
-      try:
-          subprocess.call([script_screensaver_starts,self.get_player_status()])
-      except:
-          log('ERROR executing script when screensaver starts')
+    def onPlayBackStopped(self):
+        self._sendSignal(EventNames.STOPPED)
 
-  def onScreensaverDeactivated(self):
-    log('screensaver stops')
-    global script_screensaver_stops
-    if script_screensaver_stops:
-      log('Going to execute script = "' + script_screensaver_stops + '"')
-      try:
-          subprocess.call(script_screensaver_stops)
-      except:
-          log('ERROR executing script when screensaver stops')
+    def onPlayBackPaused(self):
+        self._sendSignal(EventNames.PAUSED)
 
-  def onDatabaseUpdated(self,db):
-    log('database updated')
-    global script_db_update
-    if script_db_update:
-      log('Going to execute script = "' + script_db_update + '"')
-      try:
-          subprocess.call([script_db_update,db])
-      except:
-          log('ERROR executing script when database updates')
+    def onPlayBackResumed(self):
+        self._sendSignal(EventNames.RESUMED)
 
-  def onAbortRequested(self):
-    # Circular references don't get collected by the garbage collector, causing memory leaks
-    self.get_player_status = None
-    self.update_settings = None
-
-class MyPlayer(xbmc.Player):
-  def __init__(self):
-    xbmc.Player.__init__(self)
-    self.substrings = [ '-trailer', 'http://' ]
-
-  def playing_status(self):
-    if self.isPlaying():
-      return 'status=playing' + ';' + self.playing_type()
-    else:
-      return 'status=stopped'
-
-  def playing_type(self):
-    type = 'unkown'
-    if (self.isPlayingAudio()):
-      type = "music"  
-    else:
-      if xbmc.getCondVisibility('VideoPlayer.Content(movies)'):
-        filename = ''
-        isMovie = True
-        try:
-          filename = self.getPlayingFile()
-        except:
-          pass
-        if filename != '':
-          for string in self.substrings:
-            if string in filename:
-              isMovie = False
-              break
-        if isMovie:
-          type = "movie"
-      elif xbmc.getCondVisibility('VideoPlayer.Content(episodes)'):
-        # Check for tv show title and season to make sure it's really an episode
-        if xbmc.getInfoLabel('VideoPlayer.Season') != "" and xbmc.getInfoLabel('VideoPlayer.TVShowTitle') != "":
-           type = "episode"
-    return 'type=' + type
-
-  def playing_filename(self):
-      filename = ''
-      try:
-          filename = self.getPlayingFile()
-      except:
-          pass
-      return 'filename=' + filename
-
-  def onPlayBackStarted(self):
-    log('player starts')
-    global script_player_starts
-    if script_player_starts:
-      log('Going to execute script = "' + script_player_starts + '"')
-      try:
-          subprocess.call([script_player_starts,self.playing_type()])
-      except:
-          log('ERROR executing script when player starts')
-
-  def onPlayBackEnded(self):
-    self.onPlayBackStopped()
-
-  def onPlayBackStopped(self):
-    log('player stops')
-    global script_player_stops
-    if script_player_stops:
-      log('Going to execute script = "' + script_player_stops + '"')
-      try:
-          subprocess.call([script_player_stops,self.playing_type()])
-      except:
-          log('ERROR executing script when player stops')
-
-  def onPlayBackPaused(self):
-    log('player pauses')
-    global script_player_pauses
-    if script_player_pauses:
-      log('Going to execute script = "' + script_player_pauses + '"')
-      try:
-          subprocess.call([script_player_pauses,self.playing_type()])
-      except:
-          log('ERROR executing script when player pauses')
-
-  def onPlayBackResumed(self):
-    log('player resumes')
-    global script_player_resumes
-    if script_player_resumes:
-      log('Going to execute script = "' + script_player_resumes + '"')
-      try:
-          subprocess.call([script_player_resumes,self.playing_type()])
-      except:
-          log('ERROR executing script when player resumes')
-
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     log('script version %s started' % __addonversion__)
     Main()
     log('script version %s stopped' % __addonversion__)
